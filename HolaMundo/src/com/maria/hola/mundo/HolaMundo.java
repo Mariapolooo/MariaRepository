ddeed8ce3938557655f96dee4f15029c9cba7f77
MEMORY SISTEMAS <MEMORY SISTEMAS@192.168.1.188>
2023-05-03 21:18:08 +0200

    Primer Commit del Hola Mundo

---
+++ HolaMundo/src/com/maria/hola/mundo/HolaMundo.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.maria.hola.mundo;
+
+/**
+ * @author MEMORY SISTEMAS
+ *
+ */
+public class HolaMundo {
+
+	/**
+	 * 
+	 */
+	public HolaMundo() {
+		// TODO IBR revisar esta función
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+
+	}
+
+}
