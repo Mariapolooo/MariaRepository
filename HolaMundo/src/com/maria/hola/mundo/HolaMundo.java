69076286afa6d2b5f81fb2468bed9791ab7fac82
María P.B <maria.polo.b@gmail.com>
2023-05-03 22:25:53 +0200

    Ejemplo if, for, while

--- HolaMundo/src/com/maria/hola/mundo/HolaMundo.java
+++ HolaMundo/src/com/maria/hola/mundo/HolaMundo.java
@@ -22,6 +22,30 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Adios Mundo");
+		
+		// Ejemplo for
+		for (int i=0; i<=10; i++) {
+			System.out.println(i);
+		}
+		
+		// Ejemplo de while
+		int i = 0;
+		while (i<=10) {
+			System.out.println(i);
+			i++;
+		}
+		
+		// Ejemplo de if operacion para averiguar si un nº es par o impar (dividiendo entre 2 y quedándonos con el resto: %). 
+		
+		for (int j=0; j<=10; j++) {
+			int resto = j % 2; 
+			if (resto == 0) {				
+				System.out.println("Valor j: " + j + " Resto: " + resto + " -> ES PAR");
+			}
+			else {
+				System.out.println("Valor j: " + j + " Resto: " + resto + " -> ES IMPAR");
+			}
+		}
 
 	}
 
